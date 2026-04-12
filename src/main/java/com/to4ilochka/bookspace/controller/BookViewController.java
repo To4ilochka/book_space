525cec6ebeb3d0a2a974d6550cfcb3c1656c6af9
To4ilochka <bogdan.goldenn@gmail.com>
2026-04-13 01:18:53 +0300

    One thymeleaf page added

--- src/main/java/com/to4ilochka/bookspace/controller/BookViewController.java
+++ src/main/java/com/to4ilochka/bookspace/controller/BookViewController.java
@@ -2,6 +2,7 @@ package com.to4ilochka.bookspace.controller;
 
 import com.to4ilochka.bookspace.dto.book.BookShortResponse;
 import com.to4ilochka.bookspace.dto.common.PagedResponse;
+import com.to4ilochka.bookspace.model.enums.AgeGroup;
 import com.to4ilochka.bookspace.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -21,21 +22,22 @@ public class BookViewController {
 
     @GetMapping
     public String getBooksPage(@RequestParam(required = false) String keyword,
+                               @RequestParam(required = false) AgeGroup ageGroup,
                                @PageableDefault(size = 12, sort = "name") Pageable pageable,
                                Model model) {
-//        PagedResponse<BookShortResponse> response = (keyword != null && !keyword.isBlank())
-//                ? bookService.getBooksByKeyword(keyword, pageable)
-//                : bookService.getBooks(pageable);
-//
-//        model.addAttribute("books", response.content());
-//        model.addAttribute("currentPage", pageable.getPageNumber());
-//        model.addAttribute("totalPages", response.totalPages());
-//        model.addAttribute("keyword", keyword);
-//
-//        String sortString = pageable.getSort().toString().replace(": ", ",").toLowerCase();
-//        model.addAttribute("currentSort", sortString);
+        PagedResponse<BookShortResponse> response = (keyword != null && !keyword.isBlank())
+                ? bookService.getBooksByKeywordAndAgeGroup(keyword, ageGroup, pageable)
+                : bookService.getBooks(ageGroup, pageable);
+
+        model.addAttribute("books", response.content());
+        model.addAttribute("currentPage", pageable.getPageNumber());
+        model.addAttribute("totalPages", response.totalPages());
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("ageGroup", ageGroup);
+
+        String sortString = pageable.getSort().toString().replace(": ", ",").toLowerCase();
+        model.addAttribute("currentSort", sortString);
 
         return "books/books";
     }
 }
-
